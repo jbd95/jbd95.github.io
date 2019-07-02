ac70aa7997fc72206602f902b7786635b60c080d
jbd95 <james.brady2@mavs.uta.edu>
2019-07-01 23:27:25 -0500

    Updates

---
+++ precache-manifest.79a36d5a12c9ae198ed64bcd45df0514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d8bb8baceef2b861e1bb8e83376667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb0bc4cac828e495837",
+    "url": "/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "ed45185b5b7418bd91db",
+    "url": "/static/css/main.66d43f54.chunk.css"
+  },
+  {
+    "revision": "4cb0bc4cac828e495837",
+    "url": "/static/js/2.3afbdc42.chunk.js"
+  },
+  {
+    "revision": "ed45185b5b7418bd91db",
+    "url": "/static/js/main.05331ee8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
