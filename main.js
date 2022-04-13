a611c98dd5a8c791c62a370969ac6a1a84904566
James Brady <jbrady9559@gmail.com>
2022-04-12 22:04:49 -0700

    Add support for rendering pictures

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 import CanvasWrapper from "./lib/canvas.js";
+import Components from "./lib/components.js";
 import Engine from "./lib/engine.js";
 import Font from "./lib/font.js";
 import Physics from "./lib/physics.js";
+import Picture from "./lib/picture.js";
 import Pixel from "./lib/pixel.js";
 import Renderer from "./lib/renderer.js"
 import Status from "./lib/status.js";
 
 const canvas = CanvasWrapper.fromId('canvas')
 const status = new Status();
-const physics = new Physics(canvas, status);
-const renderer = new Renderer(canvas);
+const components = new Components();
+const physics = new Physics(canvas, components, status);
+const renderer = new Renderer(canvas, components);
 
-const engine = new Engine(physics, renderer, status);
+const engine = new Engine(physics, renderer, components, status);
 
 const enableButton = document.getElementById('enabler')
 
@@ -38,21 +41,14 @@ function animationControls_EnableDisable_clickHandler() {
     .addEventListener("click", animationControls_Reset_clickHandler);
   
 
-// class Test {
-//     update() {
-//         canvas.clearBackground();
-//         canvas.ctx.fillStyle = 'white'
-//         canvas.ctx.beginPath();
-//         canvas.ctx.rect(Math.random() * 20, Math.random() * 20, 100, 100)
-//         canvas.ctx.stroke();
-//     }
-// }
-// const t = new Test();
-// engine.addComponent(t);
-
 const font = new Font('Times New Roman', 30, canvas);
 font.addText('James Brady', 'white', 7);
 font.addText('Software Engineer', '#D4C2FC', 4);
 
-engine.addComponents(font.getPixels());
+const picture = new Picture('./resources/mav.png',
+  500, 500, 0.2, { x: 0, y: 0 }, canvas);
+
+//picture.calculate(components);
+
+components.addComponents(font.getPixels());
 engine.start();
